f780843bf31603504273024869194564e8503101
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2011-02-20 15:14:38 +0000

    arg inherits from webserver and we ensure php-apc and gd are present

--- site.pp
+++ site.pp
@@ -4,8 +4,22 @@ node default {
 
 }
 
-node 'argentina.thirdsectordesign.org' {
+node webserver inherits default {
 
     include apache, php, mysql
+    package { "php-apc" : ensure => present }
+    package { "php-gd" : ensure => present }
+    
+    # need to work out how to include mod_ssl and mod_rewrite
+    # 
+    # could be like this: https://github.com/puppet-modules/puppet-apache/raw/master/manifests/init.pp
+    # 
+    # or maybe there is the 42 way of doing it
 
 }
+
+node 'argentina.thirdsectordesign.org' inherits webserver {
+}
+
+
+
